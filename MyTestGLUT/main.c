c4ac0dfc910cfebb4b1410a9e7b4187dc08b5cdf
User1 <user1@user1s-mac-mini.local>
2016-01-10 21:13:12 +0900

    Initial Commit

---
+++ MyTestGLUT/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MyTestGLUT
+//
+//  Created by User1 on 1/10/16.
+//  Copyright © 2016 User1. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
